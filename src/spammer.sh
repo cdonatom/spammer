346ca373a189e11f8b4ddc2b46c91501527afef6
chriswheeler-redhat <chwheele@redhat.com>
2023-05-31 12:12:25 -0700

    added LISTEN var to toggle listening on/off

--- src/spammer.sh
+++ src/spammer.sh
@@ -4,7 +4,14 @@ BASEDIR=`dirname $0`
 
 $BASEDIR/spammer $MAX_MEM $PER_MEM $NUM_THREADS $LOG_LEVEL $PER_CPU &
 
-while true
-do
-  /usr/bin/ncat -l $LISTEN_PORT < $BASEDIR/http.response
-done
+if [[ $LISTEN > 0 ]]
+then
+  echo "Listening on port $LISTEN_PORT"
+
+  while true
+  do
+    /usr/bin/ncat -l $LISTEN_PORT < $BASEDIR/http.response
+  done
+else
+  sleep infinity
+fi
